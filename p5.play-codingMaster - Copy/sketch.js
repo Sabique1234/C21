c706a3bd9254748a646a5183661235f4c90259bd
Sabique1234 <66312211+Sabique1234@users.noreply.github.com>
2020-06-07 16:24:10 +0530

    Add files via upload

---
+++ p5.play-codingMaster - Copy/sketch.js
@@ -0,0 +1,58 @@
+var wall,thickness;
+var bullet,speed,weight;
+
+function setup() 
+{
+  createCanvas(1600,400);
+
+  thickness=random(22,83)
+
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=(80,80,80);
+
+  bullet=createSprite(50,200,50,50);
+  bullet.shapeColor=(0,0,0);
+  
+  speed=random(220,350)
+  weight=random(30,50)
+}
+
+function draw() 
+{
+  background(255,255,255); 
+  
+  bullet.velocityX=speed;
+
+  if (hasCollided(bullet,wall))
+  {
+    bullet.velocityX=0;
+
+    var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+
+    if (damage>10)
+    {
+      wall.shapeColor=color(255,0,0);
+    }
+    if (damage<10)
+    {
+      wall.shapeColor=color(0,255,0);
+    }
+  }
+  
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall)
+{
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+ 
